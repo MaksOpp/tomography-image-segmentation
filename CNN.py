aabd32ce1349d56d5dbd759e9f6d8c75a50c2a7e
Maks Operlejn <maksymilian.operlejn@jit.team>
2021-08-26 14:05:28 +0200

    PLEASE

--- CNN.py
+++ CNN.py
@@ -72,66 +72,79 @@ plt.imshow(framObjTrain['mask'][55])
 plt.title('Mask')
 plt.show()
 
-# In[6]:
 
-#Build the model
-# inputs = tf.keras.layers.Input((512, 512, 1))
-# s = tf.keras.layers.Lambda(lambda x: x / 256)(inputs)
+def Conv2dBlock(inputTensor, numFilters, kernelSize = 3, doBatchNorm = True):
+    #first Conv
+    x = tf.keras.layers.Conv2D(filters = numFilters, kernel_size = (kernelSize, kernelSize),
+                              kernel_initializer = 'he_normal', padding = 'same') (inputTensor)
+    
+    if doBatchNorm:
+        x = tf.keras.layers.BatchNormalization()(x)
+        
+    x =tf.keras.layers.Activation('relu')(x)
+    
+    #Second Conv
+    x = tf.keras.layers.Conv2D(filters = numFilters, kernel_size = (kernelSize, kernelSize),
+                              kernel_initializer = 'he_normal', padding = 'same') (x)
+    if doBatchNorm:
+        x = tf.keras.layers.BatchNormalization()(x)
+        
+    x = tf.keras.layers.Activation('relu')(x)
+    
+    return x
 
-# #Contraction path
-# c1 = tf.keras.layers.Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(s)
-# c1 = tf.keras.layers.Dropout(0.1)(c1)
-# c1 = tf.keras.layers.Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c1)
-# p1 = tf.keras.layers.MaxPooling2D((2, 2))(c1)
 
-# c2 = tf.keras.layers.Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p1)
-# c2 = tf.keras.layers.Dropout(0.1)(c2)
-# c2 = tf.keras.layers.Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c2)
-# p2 = tf.keras.layers.MaxPooling2D((2, 2))(c2)
- 
-# c3 = tf.keras.layers.Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p2)
-# c3 = tf.keras.layers.Dropout(0.2)(c3)
-# c3 = tf.keras.layers.Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c3)
-# p3 = tf.keras.layers.MaxPooling2D((2, 2))(c3)
- 
-# c4 = tf.keras.layers.Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p3)
-# c4 = tf.keras.layers.Dropout(0.2)(c4)
-# c4 = tf.keras.layers.Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c4)
-# p4 = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(c4)
- 
-# c5 = tf.keras.layers.Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p4)
-# c5 = tf.keras.layers.Dropout(0.3)(c5)
-# c5 = tf.keras.layers.Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c5)
+# Now defining Unet 
+def GiveMeUnet(inputImage, numFilters = 16, droupouts = 0.1, doBatchNorm = True):
+    # defining encoder Path
+    c1 = Conv2dBlock(inputImage, numFilters * 1, kernelSize = 3, doBatchNorm = doBatchNorm)
+    p1 = tf.keras.layers.MaxPooling2D((2,2))(c1)
+    p1 = tf.keras.layers.Dropout(droupouts)(p1)
+    
+    c2 = Conv2dBlock(p1, numFilters * 2, kernelSize = 3, doBatchNorm = doBatchNorm)
+    p2 = tf.keras.layers.MaxPooling2D((2,2))(c2)
+    p2 = tf.keras.layers.Dropout(droupouts)(p2)
+    
+    c3 = Conv2dBlock(p2, numFilters * 4, kernelSize = 3, doBatchNorm = doBatchNorm)
+    p3 = tf.keras.layers.MaxPooling2D((2,2))(c3)
+    p3 = tf.keras.layers.Dropout(droupouts)(p3)
+       
+    c4 = Conv2dBlock(p3, numFilters * 8, kernelSize = 3, doBatchNorm = doBatchNorm)
+    p4 = tf.keras.layers.MaxPooling2D((2,2))(c4)
+    p4 = tf.keras.layers.Dropout(droupouts)(p4)
+    
+    c5 = Conv2dBlock(p4, numFilters * 16, kernelSize = 3, doBatchNorm = doBatchNorm)
+    
+    # defining decoder path
+    u6 = tf.keras.layers.Conv2DTranspose(numFilters*8, (3, 3), strides = (2, 2), padding = 'same')(c5)
+    u6 = tf.keras.layers.concatenate([u6, c4])
+    u6 = tf.keras.layers.Dropout(droupouts)(u6)
+    c6 = Conv2dBlock(u6, numFilters * 8, kernelSize = 3, doBatchNorm = doBatchNorm)
+       
+    u7 = tf.keras.layers.Conv2DTranspose(numFilters*4, (3, 3), strides = (2, 2), padding = 'same')(c6)
+    
+    u7 = tf.keras.layers.concatenate([u7, c3])
+    u7 = tf.keras.layers.Dropout(droupouts)(u7)
+    c7 = Conv2dBlock(u7, numFilters * 4, kernelSize = 3, doBatchNorm = doBatchNorm)
+    
+    u8 = tf.keras.layers.Conv2DTranspose(numFilters*2, (3, 3), strides = (2, 2), padding = 'same')(c7)
+    u8 = tf.keras.layers.concatenate([u8, c2])
+    u8 = tf.keras.layers.Dropout(droupouts)(u8)
+    c8 = Conv2dBlock(u8, numFilters * 2, kernelSize = 3, doBatchNorm = doBatchNorm)
+    
+    u9 = tf.keras.layers.Conv2DTranspose(numFilters*1, (3, 3), strides = (2, 2), padding = 'same')(c8)
+    u9 = tf.keras.layers.concatenate([u9, c1])
+    u9 = tf.keras.layers.Dropout(droupouts)(u9)
+    c9 = Conv2dBlock(u9, numFilters * 1, kernelSize = 3, doBatchNorm = doBatchNorm)
+        
+    output = tf.keras.layers.Conv2D(1, (1, 1), activation = 'sigmoid')(c9)
+    model = tf.keras.Model(inputs = [inputImage], outputs = [output])
+    return model
 
-# #Expansive path 
-# u6 = tf.keras.layers.Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(c5)
-# u6 = tf.keras.layers.concatenate([u6, c4])
-# c6 = tf.keras.layers.Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u6)
-# c6 = tf.keras.layers.Dropout(0.2)(c6)
-# c6 = tf.keras.layers.Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c6)
- 
-# u7 = tf.keras.layers.Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(c6)
-# u7 = tf.keras.layers.concatenate([u7, c3])
-# c7 = tf.keras.layers.Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u7)
-# c7 = tf.keras.layers.Dropout(0.2)(c7)
-# c7 = tf.keras.layers.Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c7)
- 
-# u8 = tf.keras.layers.Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(c7)
-# u8 = tf.keras.layers.concatenate([u8, c2])
-# c8 = tf.keras.layers.Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u8)
-# c8 = tf.keras.layers.Dropout(0.1)(c8)
-# c8 = tf.keras.layers.Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c8)
- 
-# u9 = tf.keras.layers.Conv2DTranspose(16, (2, 2), strides=(2, 2), padding='same')(c8)
-# u9 = tf.keras.layers.concatenate([u9, c1], axis=3)
-# c9 = tf.keras.layers.Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u9)
-# c9 = tf.keras.layers.Dropout(0.1)(c9)
-# c9 = tf.keras.layers.Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c9)
- 
-# outputs = tf.keras.layers.Conv2D(1, (1, 1), activation='sigmoid')(c9)
 
-# model = tf.keras.Model(inputs=[inputs], outputs=[outputs])
-model = unet()
+inputs = tf.keras.layers.Input((512, 512, 1))
+model = GiveMeUnet(inputs, droupouts= 0.07)
+
 # prefix_path = "checkpoints\\"
 # if not os.path.exists(prefix_path):
 #     os.makedirs(prefix_path)
@@ -155,13 +168,13 @@ model = unet()
 
 # In[14]:
 
-initial_epoch = 0
+#initial_epoch = 0
 # if(exists):
 #     initial_epoch = int(latest.replace(prefix_path, "").replace(".hdf5", ""))
 #     print("Start from epoch", initial_epoch)
 
 #retVal = model.fit(np.array(framObjTrain['img']), np.array(framObjTrain['mask']), epochs = 100, verbose = 1, validation_split = 0.1, callbacks=callbacks_list, initial_epoch=initial_epoch)
-retVal = model.fit(np.array(framObjTrain['img']), np.array(framObjTrain['mask']), epochs = 100, verbose = 1, validation_split = 0.1, initial_epoch=initial_epoch)
+retVal = model.fit(np.array(framObjTrain['img']), np.array(framObjTrain['mask']), epochs = 10, verbose = 1, validation_split = 0.1) 
 plt.figure()
 plt.plot(retVal.history['accuracy'])
 plt.plot(retVal.history['val_accuracy'])
